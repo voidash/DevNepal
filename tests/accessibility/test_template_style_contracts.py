54ed2fec5612d5c32d14875b92a4f8e496d048e3
ashish <ashish.thapa477@gmail.com>
2026-09-06 03:53:29 +0545

    Bust public stylesheet cache

--- tests/accessibility/test_template_style_contracts.py
+++ tests/accessibility/test_template_style_contracts.py
@@ -68,7 +68,7 @@ def test_base_shell_uses_the_prototype_navigation_and_design_tokens():
     tokens_css = (root / "static/src/tokens.css").read_text()
 
     assert "href=\"{% static 'vendor/primer/primer.css' %}\"" in base
-    assert "href=\"{% static 'src/devnepal.css' %}?v=20260906-madan-demo3\"" in base
+    assert "href=\"{% static 'src/devnepal.css' %}?v=20260906-demo4\"" in base
     assert "href=\"{% static 'images/devnepal-mark.svg' %}\"" in base
     assert 'class="btn dn-skip-link" href="#main-content"' in base
     assert "नेपाल सरकार · Government of Nepal" in base
